bedc473cf4e34898ed47e57de0bde1e5f22af16f
Karlis Gangis <Knagis@users.noreply.github.com>
2017-05-25 23:31:53 +0300

    Working example with monaco editor and custom libs

---
+++ lib.game.d.ts
@@ -0,0 +1,21 @@
+
+/**
+ * Reads a single line from the input. The result does not include the newline char.
+ */
+declare function readline(): string;
+/**
+ * Writes one or more arguments to the output. Adds a trailing newline. 
+ * If multiple arguments are specified, no separator characters are added.
+ */
+declare function writeline(...args: Array<number|string>): void;
+
+interface Console {
+    log(...args: any[]): void;
+    warn(...args: any[]): void;
+    error(...args: any[]): void;
+}
+
+/**
+ * Gives access to write log to the browser console.
+ */
+declare var console: Console;
