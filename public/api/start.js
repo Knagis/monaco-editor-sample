3596b19794d50faa22ce6d46a2a36835ac80f78f
Karlis Gangis <Knagis@users.noreply.github.com>
2017-05-26 23:55:06 +0300

    Set the duration of the sample to 10min

--- public/api/start.js
+++ public/api/start.js
@@ -24,7 +24,7 @@ writeline(total);
         { input: ["0"], output: ["0"] },
     ],
     target: 100,
-    duration: 50,
+    duration: 600,
     description: `
 Shorten the given code as much as possible within the given time. Everything can
 go as long as the tests are still green.
